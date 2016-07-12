47b02571712aaf587a346761acb7bbeae2230d85
pasupuletics <chandrasekhar.pasupuleti@gmail.com>
2016-07-13 02:38:49 +0530

    Reverted env variable

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ var env,
 	outputDir,
 	sassStyle;
 
-env = 'production'/*process.env.NODE_ENV || 'production'*/;
+env = process.env.NODE_ENV || 'development';
 gutil.log(env);
 if(env === 'development') {
 	outputDir = 'builds/development/';
